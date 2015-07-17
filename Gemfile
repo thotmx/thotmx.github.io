f91d14c6c7bf099c26a03aac055dfcac412475ac
thotmx <hermes.o.r@gmail.com>
2015-07-17 13:30:24 -0500

    Changing layout for article, and adding disqus

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'http://rubygems.org'
 gem "middleman", "~> 3.3.12"
 gem "middleman-blog", "~> 3.5.3"
 gem "middleman-syntax"
+gem "middleman-deploy"
+gem "middleman-disqus"
 gem "pygments.rb"
 
 group :development do
--- config.rb
+++ config.rb
@@ -12,8 +12,8 @@ activate :blog do |blog|
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
+  blog.layout = "article"
+  blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
@@ -104,6 +104,16 @@ end
 
 configure :development do
   activate :livereload
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = 'master'
+  deploy.build_before = true
+end
+
+activate :disqus do |d|
+  d.shortname = "notsohermetic"
 end
 
 activate :syntax
